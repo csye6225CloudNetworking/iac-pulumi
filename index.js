49b48178a253bddff5240fbf4766f1db027a58f4
Rutuja <patil.rut@northeastern.edu>
2023-10-18 20:37:01 -0400

    updated pulumi

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const pulumi = require("@pulumi/pulumi");
 const aws = require("@pulumi/aws");
+const logFile = 'pulumi_log.txt';
+const fs = require('fs');
+// Function to log to both console and the log file
+function logMessage(message) {
+    console.log(message);
+    fs.appendFileSync(logFile, message + '\n');
+}
 
+// Log a message to the console and log file
+logMessage("This is a log message.");
 const config = new pulumi.Config();
 
 
@@ -9,9 +18,6 @@ const vpcCidrBlock = config.require("vpcCidrBlock");
 const cidrBlock = config.require("cidrBlock");
 const publicSubnets = [];
 const privateSubnets = [];
-
-
-
 
 
 const subnetSuffix = config.require("subnetSuffix");
@@ -43,11 +49,32 @@ function getFirstNAvailabilityZones(data, n) {
 
     if (availableAZCount >= n) {
         return data.names.slice(0, n);
-    } 
+    }
     else {
-    
+
         return data.names;
     }
+}
+
+async function fetchAmi(){
+    const ami = await aws.ec2.getAmi({
+        owners: ["454063085085"], 
+        mostRecent: true,
+        filters: [  
+            {
+                name: "root-device-type",
+                values: ["ebs"],
+            },
+            {
+                name: "state",
+                values: ["available"],
+            },
+        ],
+
+    })
+    return ami.id
+
+    
 }
 
 const availabilityZoneNames = []; // Initialize an array to store availability zone names
@@ -78,7 +105,7 @@ aws.getAvailabilityZones({ state: `${state}` }).then(data => {
         // Create public and private subnets using aws.ec2.Subnet
         const publicSubnet = new aws.ec2.Subnet(`${public_Subnet}-${az}-${i}`, {
             vpcId: vpc.id,
-            cidrBlock: calculateCidrBlock(i,`${publicSta}`),
+            cidrBlock: calculateCidrBlock(i, `${publicSta}`),
             availabilityZone: az,
             mapPublicIpOnLaunch: true,
             tags: {
@@ -88,7 +115,7 @@ aws.getAvailabilityZones({ state: `${state}` }).then(data => {
 
         const privateSubnet = new aws.ec2.Subnet(`${private_Subnet}-${az}-${i}`, {
             vpcId: vpc.id,
-            cidrBlock: calculateCidrBlock(i,`${privateSta}`),
+            cidrBlock: calculateCidrBlock(i, `${privateSta}`),
             availabilityZone: az,
             tags: {
                 Name: `${private_Subnet}`,
@@ -118,25 +145,137 @@ aws.getAvailabilityZones({ state: `${state}` }).then(data => {
         gatewayId: internetGateway.id,
     });
 
+  
+
     // Associate the public subnets with the public route table
-    publicSubnets.forEach((subnet,i) => {
+    publicSubnets.forEach((subnet, i) => {
         new aws.ec2.RouteTableAssociation(`${public_route_association}-${subnet.availabilityZone}-${i}`, {
             subnetId: subnet.id,
             routeTableId: publicRouteTable.id,
-            tags:{
+            tags: {
                 Name: `${public_route_association}`,
             },
         });
     });
 
     // Associate the private subnets with the private route table
-    privateSubnets.forEach((subnet,i) => {
+    privateSubnets.forEach((subnet, i) => {
         new aws.ec2.RouteTableAssociation(`${private_route_association}-${subnet.availabilityZone}-${i}`, {
             subnetId: subnet.id,
             routeTableId: privateRouteTable.id,
-            tags:{
+            tags: {
                 Name: `${private_route_association}`,
             },
         });
     });
+
+    
+
+    /* const ami = aws.ec2.Ami.get("customAmi", {
+        owners: ["454063085085"], // Replace with the owner account ID
+        mostRecent: true,
+        filters: [  
+            {
+                name: "root-device-type",
+                values: ["ebs"],
+            },
+            {
+                name: "architecture",
+                values: ["x86_64"],
+            },
+            {
+                name: "virtualization-type",
+                values: ["hvm"],
+            },
+            {
+                name: "platform-details",
+                values: ["Linux/UNIX"],
+            },
+            {
+                name: "state",
+                values: ["available"],
+            },
+        ],
+    }); */
+
+  /*   ami.then(result=> logMessage(result[0]));
+ logMessage(ami); */
+    // Create an Application Security Group
+
+    const applicationSecurityGroup = new aws.ec2.SecurityGroup("applicationSecurityGroup", {
+        description: "Application Security Group for web applications",
+        vpcId: vpc.id, // Replace with your VPC ID
+        ingress: [
+            {
+                protocol: "tcp",
+                fromPort: 22,  // SSH
+                toPort: 22,
+                cidrBlocks: ["0.0.0.0/0"], // Allow SSH from anywhere
+            },
+            {
+                protocol: "tcp",
+                fromPort: 80,  // HTTP
+                toPort: 80,
+                cidrBlocks: ["0.0.0.0/0"], // Allow HTTP from anywhere
+            },
+            {
+                protocol: "tcp",
+                fromPort: 443, // HTTPS
+                toPort: 443,
+                cidrBlocks: ["0.0.0.0/0"], // Allow HTTPS from anywhere
+            },
+            {
+                protocol: "tcp",
+                fromPort: 8080, // Replace with your application port
+                toPort: 8080,
+                cidrBlocks: ["0.0.0.0/0"], // Allow your application traffic from anywhere
+            },
+        ],
+    });
+
+    const instanceType = "t2.micro"; // Replace with your desired instance type
+   // const ami = "ami-0fe212c33758905ab"; // Replace with the desired AMI ID
+   
+    const keyName = "ec2-key"; // Replace with your EC2 key pair name
+    const sshKey = new aws.ec2.KeyPair("mySshKey", {
+        keyName: keyName,
+        //publicKey: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDD2rvVnzmnagrWL2uVvRacH4/v45g7CCU7ZGgvZ1vgX2ZdoUBwsmVcrd0kFioimKQpbFnIEY4sTZthCz9LUEACeSb157nb3PDysvjGomlGCGlG4qaaI/l3/0gWxxoz7PUHTEenborGwjOdn8YJnMshbf7410MOqTdzyx/7M3DIIZVH+qWuKJy8KP/q+jym7MV1QzwikqqJeTv15nHme5UgGp68zXxi9u5s6sIB/kQUSxXOmfj9nk9lSgxKXNVH/9nmhreuR8WbY4WOWNPyhMphBYvfn6UZCK1wrfinJ0nUNqHOWfzKAa0DpBgrabq8QuALIcOTXuXUuSbI1EjVSprtKJE1onFEZfXhlcBpEq2utz88JlxbHG7Jyvk7ciEfPKdGax1qIl9GxejKm0mOJrzQymqhxNgZq7NYGaKlL3DQw3MirY7LHfZsOvRlXjuBFHMiF/RvrrWx88rwfH0XywkbRCC+iWy87NNuu/cvm7muFCVN+TUpO0pj0VvSFSIa99s= rutuj@HP", // Replace with your public SSH key content
+        publicKey: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCgczCeN4L3ebjxb1Gx3V1LMwCPT3ScEu+vSKKuxQGCpfw1jwKUOzO2iXwN2hQLO1aiC38ISCwZyUuNlMcU1biv4oDfZBoQ+10Mwl5dwtcMNr+UATr9nZPimOvKLNyPMFZKTO8FMf1aGIhSTE4zvSYzfIcv+Bzx0Xzg/OQbfpktWUSou675P8EkBlwzYEZe+o55Lmz76yeP3l6Tu/uhfiJO6NpDr/cUEwpg+Thv7bTd8dfii4qb+FhuGCOhaZNcv5xSiF14jI8XQ8bB55WLDfg9+B1WJgkM3OSCiBReBHRlFZsaTXp5TKCCPIA543ZjvtRVCouhk55E69X1cxK3dzku3THTa1q4i81Ew9IO9GrtFftRPCZOYixDtJHhtdk+e8ByLZHPYIAayQKZ7EQJzX4abNl/oqcYuQKx4hkH1qppjqJcsVq6Mg4uOC7yyxZhljiysEvpo8KvlWZ5CwX8ucR64Pg6ezkqi6oxQFZIpkp+8VP558+YEUww9ZS+lHtWO90= rutuj@HP",
+        /* userData: `
+        #!/bin/bash
+        amazon-linux-extras install nginx1
+        amazon-linux-extras install nginx
+        systemctl enable nginx
+        systemctl start nginx
+        `, */
+        userData: `
+        #!/bin/bash
+        "cd /webapp",
+        "npm install",
+        `,
+
+    });
+    // Define the EC2 instance
+
+   
+    const ec2Instance = new aws.ec2.Instance("myEC2Instance", {
+        instanceType: instanceType,
+        ami: fetchAmi(),
+
+        keyName: sshKey.keyName,
+        vpcSecurityGroupIds: [applicationSecurityGroup.id], // Attach the Application Security Group created in the previous step
+        subnetId: publicSubnets[0].id, // Replace with the subnet ID where you want to launch the EC2 instance
+        rootBlockDevice: {
+            volumeSize: 30, // Size of the root EBS volume (in GB)
+            deleteOnTermination: true, // Automatically delete the root EBS volume when the EC2 instance is terminated
+        },
+        tags: {
+            Name: "MyEC2Instance", // Replace with a suitable name
+        },
+    });
+
+
+
+
+
 });
