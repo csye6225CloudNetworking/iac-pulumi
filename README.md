5c1bd43691c0449bd4435e8c1363bde3231795f2
Rutuja <patil.rut@northeastern.edu>
2023-11-06 15:10:47 -0500

    updated

--- README.md
+++ README.md
@@ -1,53 +1,23 @@
-# iac-pulumi
 
-# Infrastructure as a Service (IaaS) with Pulumi
-
-## Table of Contents
-
-- [iac-pulumi](#iac-pulumi)
-- [Infrastructure as a Service (IaaS) with Pulumi](#infrastructure-as-a-service-iaas-with-pulumi)
-  - [Table of Contents](#table-of-contents)
-  - [Introduction](#introduction)
-  - [Prerequisites](#prerequisites)
-  - [Getting Started](#getting-started)
-  - [Creating Your IaaS Stack](#creating-your-iaas-stack)
-  - [Creating a Virtual Private Cloud (VPC)](#creating-a-virtual-private-cloud-vpc)
-  - [Deploying Your IaaS](#deploying-your-iaas)
-  - [Updating Your IaaS](#updating-your-iaas)
-  - [Destroying Your IaaS](#destroying-your-iaas)
-  - [Conclusion](#conclusion)
-
-## Introduction
+## Intro
 
 Briefly introduce the concept of Infrastructure as a Service (IaaS) and explain the purpose of this README.
 
-## Prerequisites
-
-List the prerequisites that users need to have in place before they can start using Pulumi for IaaS.
-
-- Pulumi Installation
-- Cloud Provider Account
-- Programming Language
-
-## Getting Started
-
 Explain the initial setup steps:
 
-1. **Initialize a New Pulumi Project**: Provide the command for initializing a new Pulumi project.
+1. **Initialize**: Begin by initializing a new Pulumi project using the command pulumi new. This sets up the basic project structure and configuration.
 
-2. **Define Your Infrastructure**: Explain where to define your infrastructure components and provide a brief overview of what to include.
+2. **Infrastructure**: Define your infrastructure components within the project. Ensure that you understand what components you need for your IaaS setup.
+3. **Required Packages**:  If your project requires additional packages or dependencies, install them as per your chosen programming language's package manager.
 
-3. **Install Required Packages**: If necessary, mention how to install required packages for your chosen programming language.
+4. **Infrastructure Code**: To match your specific requirements, you should modify the provided infrastructure code. Refer to the relevant documentation and examples for guidance on making these customizations.
 
-4. **Customize Your Infrastructure Code**: Encourage users to modify the code to match their specific requirements and point them to relevant documentation and examples.
+## IaaS Stack
 
-## Creating Your IaaS Stack
 
-Explain how to create a Pulumi stack:
+1. **Initialize a Stack**:  Initialize a new stack with pulumi stack init. Stacks are useful for managing different environments, such as development, staging, or production.
 
-1. **Initialize a Stack**: Provide the command to initialize a new stack, and explain the purpose of stacks (e.g., for different environments).
-
-2. **Set Configuration Variables**: Describe how to set configuration variables specific to your infrastructure. This can include variables like region, instance type, and storage configuration.
+2. **Configuration Variables**: Configure variables specific to your infrastructure, like region, instance types, and storage settings, using the pulumi config command. These settings will vary depending on your project.
 
 ## Creating a Virtual Private Cloud (VPC)
 
@@ -85,9 +55,3 @@ Explain how to destroy IaaS resources when they are no longer needed:
 
 - Run the `pulumi destroy` command.
 - Include a caution about the irreversible nature of this action.
-
-## Conclusion
-
-Summarize the key points of the README, encourage users to explore the official Pulumi documentation, and provide cloud provider-specific documentation for advanced features and configurations.
-
-Feel free to customize this template to match your specific use case and provide additional details as needed.
--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const pulumi = require("@pulumi/pulumi");
 const aws = require("@pulumi/aws");
+const logFile = 'pulumi_log.txt';
+const awsRoute53 = require("@pulumi/aws/route53");
+const fs = require('fs');
+// Function to log to both console and the log file
+function logMessage(message) {
+    console.log(message);
+    fs.appendFileSync(logFile, message + '\n');
+}
 
+// Log a message to the console and log file
+logMessage("This is a log message.");
 const config = new pulumi.Config();
 
 
@@ -9,9 +19,6 @@ const vpcCidrBlock = config.require("vpcCidrBlock");
 const cidrBlock = config.require("cidrBlock");
 const publicSubnets = [];
 const privateSubnets = [];
-
-
-
 
 
 const subnetSuffix = config.require("subnetSuffix");
@@ -43,11 +50,32 @@ function getFirstNAvailabilityZones(data, n) {
 
     if (availableAZCount >= n) {
         return data.names.slice(0, n);
-    } 
+    }
     else {
-    
+
         return data.names;
     }
+}
+
+async function fetchAmi(){
+    const ami = await aws.ec2.getAmi({
+        owners: ["454063085085"], 
+        mostRecent: true,
+        filters: [  
+            {
+                name: "root-device-type",
+                values: ["ebs"],
+            },
+            {
+                name: "state",
+                values: ["available"],
+            },
+        ],
+
+    })
+    return ami.id
+
+    
 }
 
 const availabilityZoneNames = []; // Initialize an array to store availability zone names
@@ -78,7 +106,7 @@ aws.getAvailabilityZones({ state: `${state}` }).then(data => {
         // Create public and private subnets using aws.ec2.Subnet
         const publicSubnet = new aws.ec2.Subnet(`${public_Subnet}-${az}-${i}`, {
             vpcId: vpc.id,
-            cidrBlock: calculateCidrBlock(i,`${publicSta}`),
+            cidrBlock: calculateCidrBlock(i, `${publicSta}`),
             availabilityZone: az,
             mapPublicIpOnLaunch: true,
             tags: {
@@ -88,7 +116,7 @@ aws.getAvailabilityZones({ state: `${state}` }).then(data => {
 
         const privateSubnet = new aws.ec2.Subnet(`${private_Subnet}-${az}-${i}`, {
             vpcId: vpc.id,
-            cidrBlock: calculateCidrBlock(i,`${privateSta}`),
+            cidrBlock: calculateCidrBlock(i, `${privateSta}`),
             availabilityZone: az,
             tags: {
                 Name: `${private_Subnet}`,
@@ -118,25 +146,299 @@ aws.getAvailabilityZones({ state: `${state}` }).then(data => {
         gatewayId: internetGateway.id,
     });
 
+  
+
     // Associate the public subnets with the public route table
-    publicSubnets.forEach((subnet,i) => {
+    publicSubnets.forEach((subnet, i) => {
         new aws.ec2.RouteTableAssociation(`${public_route_association}-${subnet.availabilityZone}-${i}`, {
             subnetId: subnet.id,
             routeTableId: publicRouteTable.id,
-            tags:{
+            tags: {
                 Name: `${public_route_association}`,
             },
         });
     });
 
     // Associate the private subnets with the private route table
-    privateSubnets.forEach((subnet,i) => {
+    privateSubnets.forEach((subnet, i) => {
         new aws.ec2.RouteTableAssociation(`${private_route_association}-${subnet.availabilityZone}-${i}`, {
             subnetId: subnet.id,
             routeTableId: privateRouteTable.id,
-            tags:{
+            tags: {
                 Name: `${private_route_association}`,
             },
         });
     });
+
+    
+
+    /* const ami = aws.ec2.Ami.get("customAmi", {
+        owners: ["454063085085"], // Replace with the owner account ID
+        mostRecent: true,
+        filters: [  
+            {
+                name: "root-device-type",
+                values: ["ebs"],
+            },
+            {
+                name: "architecture",
+                values: ["x86_64"],
+            },
+            {
+                name: "virtualization-type",
+                values: ["hvm"],
+            },
+            {
+                name: "platform-details",
+                values: ["Linux/UNIX"],
+            },
+            {
+                name: "state",
+                values: ["available"],
+            },
+        ],
+    }); */
+
+  /*   ami.then(result=> logMessage(result[0]));
+ logMessage(ami); */
+    // Create an Application Security Group
+
+    const applicationSecurityGroup = new aws.ec2.SecurityGroup("applicationSecurityGroup", {
+        description: "Application Security Group for web applications",
+        vpcId: vpc.id, // Replace with your VPC ID
+        ingress: [
+            {
+              fromPort: 22,
+              toPort: 22,
+              protocol: "tcp",
+              cidrBlocks: ["0.0.0.0/0"], // Allow SSH from anywhere
+            },
+            {
+              fromPort: 80,
+              toPort: 80,
+              protocol: "tcp",
+              cidrBlocks: ["0.0.0.0/0"], // Allow HTTP from anywhere
+            },
+            {
+              fromPort: 443,
+              toPort: 443,
+              protocol: "tcp",
+              cidrBlocks: ["0.0.0.0/0"], // Allow HTTPS from anywhere
+            },
+            {
+              fromPort: 8080,
+              toPort: 8080,
+              protocol: "tcp",
+              cidrBlocks: ["0.0.0.0/0"], // Allow your application traffic from anywhere
+            },
+          ],
+          egress: [
+            {
+              fromPort: 3306,      // Allow outbound traffic on port 3306
+              toPort: 3306,        // Allow outbound traffic on port 3306
+              protocol: "tcp",     // TCP protocol
+              cidrBlocks: ["0.0.0.0/0"],  // Allow all destinations
+            },
+         
+          ],
+    });
+
+
+    const databaseSecurityGroup = new aws.ec2.SecurityGroup('databaseSecurityGroup', {
+        description: 'Security group for RDS instances',
+        vpcId: vpc.id, 
+        ingress: [
+            {
+                fromPort: 3306, 
+                toPort: 3306,
+                protocol: "tcp",
+                securityGroups: [applicationSecurityGroup.id], 
+            },
+        ],
+        egress: [
+            {
+                fromPort: 0,
+                toPort: 0,
+                protocol: "-1",
+                cidrBlocks: ["0.0.0.0/0"],
+            },
+        ],
+        
+    });
+
+
+
+
+const dbparametergroup = new aws.rds.ParameterGroup('dbparametergroup', {
+    family: 'mysql8.0', 
+    parameters: [
+        {
+            name: 'max_connections',
+            value: '100',
+        },
+    ],
+});
+const privateSubnetIds = privateSubnets.map(subnet => subnet.id);
+const privateSubnetGroup = new aws.rds.SubnetGroup("privateSubnetGroup", {
+    subnetIds: privateSubnetIds, 
+    name: "my-private-subnet-group", 
+    description: "Private subnet group for RDS",
+});
+
+const rdsinstance = new aws.rds.Instance('rdsinstance', {
+    allocatedStorage: 20,
+    engine: 'mysql', 
+    instanceClass: 'db.t2.micro', 
+    multiAz: false,
+    name: 'csye6225',
+    username: 'csye6225',
+    password: 'rdsuser6367', 
+    parameterGroupName: dbparametergroup,
+    skipFinalSnapshot: true, 
+    vpcSecurityGroupIds: [databaseSecurityGroup.id],
+    dbSubnetGroupName: privateSubnetGroup.name, // private
+    publiclyAccessible: false,
+});
+
+    const instanceType = "t2.micro"; 
+   // const ami = "ami-0fe212c33758905ab"; // Replace with the desired AMI ID
+   
+    const keyName = "ec2-key"; // Replace with your EC2 key pair name
+    const sshKey = new aws.ec2.KeyPair("mySshKey", {
+        keyName: keyName,
+        //publicKey: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDD2rvVnzmnagrWL2uVvRacH4/v45g7CCU7ZGgvZ1vgX2ZdoUBwsmVcrd0kFioimKQpbFnIEY4sTZthCz9LUEACeSb157nb3PDysvjGomlGCGlG4qaaI/l3/0gWxxoz7PUHTEenborGwjOdn8YJnMshbf7410MOqTdzyx/7M3DIIZVH+qWuKJy8KP/q+jym7MV1QzwikqqJeTv15nHme5UgGp68zXxi9u5s6sIB/kQUSxXOmfj9nk9lSgxKXNVH/9nmhreuR8WbY4WOWNPyhMphBYvfn6UZCK1wrfinJ0nUNqHOWfzKAa0DpBgrabq8QuALIcOTXuXUuSbI1EjVSprtKJE1onFEZfXhlcBpEq2utz88JlxbHG7Jyvk7ciEfPKdGax1qIl9GxejKm0mOJrzQymqhxNgZq7NYGaKlL3DQw3MirY7LHfZsOvRlXjuBFHMiF/RvrrWx88rwfH0XywkbRCC+iWy87NNuu/cvm7muFCVN+TUpO0pj0VvSFSIa99s= rutuj@HP", // Replace with your public SSH key content
+        publicKey: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCgczCeN4L3ebjxb1Gx3V1LMwCPT3ScEu+vSKKuxQGCpfw1jwKUOzO2iXwN2hQLO1aiC38ISCwZyUuNlMcU1biv4oDfZBoQ+10Mwl5dwtcMNr+UATr9nZPimOvKLNyPMFZKTO8FMf1aGIhSTE4zvSYzfIcv+Bzx0Xzg/OQbfpktWUSou675P8EkBlwzYEZe+o55Lmz76yeP3l6Tu/uhfiJO6NpDr/cUEwpg+Thv7bTd8dfii4qb+FhuGCOhaZNcv5xSiF14jI8XQ8bB55WLDfg9+B1WJgkM3OSCiBReBHRlFZsaTXp5TKCCPIA543ZjvtRVCouhk55E69X1cxK3dzku3THTa1q4i81Ew9IO9GrtFftRPCZOYixDtJHhtdk+e8ByLZHPYIAayQKZ7EQJzX4abNl/oqcYuQKx4hkH1qppjqJcsVq6Mg4uOC7yyxZhljiysEvpo8KvlWZ5CwX8ucR64Pg6ezkqi6oxQFZIpkp+8VP558+YEUww9ZS+lHtWO90= rutuj@HP",
+      
+    });
+  
+
+    const cloudwatchAgentPolicy = new aws.iam.Policy("cloudwatchAgentPolicy", {
+        description: "Policy for CloudWatch Agent",
+        policy: {
+            Version: "2012-10-17",
+            Statement: [
+                {
+                    Action: [
+                        "cloudwatch:PutMetricData",
+                        "ec2:DescribeVolumes",
+                        "ec2:DescribeTags",
+                        "logs:DescribeLogStream",
+                        "logs:DescribeLogGroups",
+                        "logs:CreateLogGroup",
+                        "logs:CreateLogStream",
+                        "logs:PutLogEvents",
+                    ],
+                    Effect: "Allow",
+                    Resource: "*",
+                },
+            ],
+        },
+    });
+    // Create an IAM role and attach the CloudWatch Agent policy
+    const iamRole = new aws.iam.Role("CloudWatchAgentRole", {
+        assumeRolePolicy: JSON.stringify({
+            Version: "2012-10-17",
+            Statement: [
+                {
+                    Action: "sts:AssumeRole",
+                    Effect: "Allow",
+                    Principal: {
+                        Service: "ec2.amazonaws.com",
+                    },
+                },
+            ],
+        }),
+    });
+
+
+    // Attach the CloudWatch Agent policy to the IAM role
+const rolePolicyAttachment = new aws.iam.RolePolicyAttachment("cloudwatchAgentPolicyAttachment", {
+    policyArn: cloudwatchAgentPolicy.arn,
+    role: iamRole.name,
+});
+
+const instanceProfile = new aws.iam.InstanceProfile(
+    "instanceProfileName", {
+    role: iamRole.name,
+   
+});
+
+
+   
+    const ec2Instance = new aws.ec2.Instance("myEC2Instance", {
+        instanceType: instanceType,
+        ami: fetchAmi(),    
+        userDataReplaceOnChange:true,
+        iamInstanceProfile:instanceProfile,
+        userData: pulumi.interpolate  `#!/bin/bash
+        wget https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
+        sudo rpm -U amazon-cloudwatch-agent.rpm
+
+        # Configure the CloudWatch Agent
+        sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-config-wizard -m onPremise
+        sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -s -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json
+
+        # Start the CloudWatch Agent
+        sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a start
+
+        cd /home/admin/
+        chmod +w .env
+        file_to_edit=".env"  
+        new_mysql_database=${rdsinstance.dbName}
+        new_mysql_user=${rdsinstance.username}
+        new_mysql_password=${rdsinstance.password}
+        new_mysql_port=${rdsinstance.port}
+        new_mysql_host=${rdsinstance.address}
+        new_db_dialect=${rdsinstance.engine}
+        
+        if [ -f "$file_to_edit" ]; then
+           
+        > "$file_to_edit"
+        
+            # Add new key-value pairs
+            echo "MYSQL_DATABASE=$new_mysql_database" >> "$file_to_edit"
+            echo "MYSQL_USER=$new_mysql_user" >> "$file_to_edit"
+            echo "MYSQL_PASSWORD=$new_mysql_password" >> "$file_to_edit"
+            echo "MYSQL_PORT=$new_mysql_port" >> "$file_to_edit"
+            echo "MYSQL_HOST=$new_mysql_host" >> "$file_to_edit"
+            echo "DB_DIALECT=$new_db_dialect" >> "$file_to_edit"
+        
+            echo "Cleared old data in $file_to_edit and added new key-value pairs."
+        else
+            echo "File $file_to_edit does not exist."   
+        fi
+        sudo systemctl daemon-reload
+        sudo systemctl enable app.service
+        sudo systemctl start app.service
+        sudo chown -R csye6225:csye6225 /home/admin/*
+        sudo chmod -R 750 /home/admin/*
+        `.apply(s => s.trim()),
+        keyName: sshKey.keyName,
+        dependsOn: rdsinstance,
+        vpcSecurityGroupIds: [applicationSecurityGroup.id], // Attach the Application Security Group created in the previous step
+        subnetId: publicSubnets[0].id, // Replace with the subnet ID where you want to launch the EC2 instance
+        rootBlockDevice: {
+            volumeSize: 30, // Size of the root EBS volume (in GB)
+            deleteOnTermination: true, // Automatically delete the root EBS volume when the EC2 instance is terminated
+        },
+        tags: {
+            Name: "MyEC2Instance", 
+        },
+    });
+
+    //const baseDomainName = config.require("basedomain"); 
+    const baseDomainName = "dev.cloudcsye.me";
+    const zonePromise = aws.route53.getZone({ name: baseDomainName }, { async: true });
+
+    zonePromise.then(zone => {
+
+    const record = new aws.route53.Record("myRecord", {
+    zoneId: zone.zoneId, 
+    name: "",
+    type: "A",
+    ttl: 60,
+    records: [ec2Instance.publicIp],
+}, { dependsOn: [ec2Instance] });   
+});
+
 });
